362278227cf943ab0bb5eb51bf472f03b848508e
Pieter Pas <pieter.pas@outlook.com>
2017-03-24 23:40:26 +0100

    Schermpje HAL 9310

--- Arduino/OLED_schermpje/OLED_test/OLED_test.ino
+++ Arduino/OLED_schermpje/OLED_test/OLED_test.ino
@@ -3,8 +3,10 @@
 
 #include "moon1.h"
 #include "sun.h"
-#include "wifi3.h"
+//#include "wifi3.h"
 #include "wifi5.h"
+#include "A.h"
+#include "Auto.h"
 
 #include "OLED.h"
 #include "NetworkInit.h"
@@ -37,13 +39,13 @@ void setup() {
   startOTA();
 }
 
-unsigned long refresh = 5;
+unsigned long refresh = 50;
 unsigned long nextRefresh = millis();
 
 unsigned long interval = 2000;
 unsigned long nextInterval = millis();
 
-boolean lights = 0;
+int lights = 0;
 int movement = 0;
 
 void loop() {
@@ -51,26 +53,28 @@ void loop() {
   if (millis() > nextRefresh) {
     display.clear();
     float voltage = analogRead(A0) / ResRatio / 1024.0;
-    display.drawString(DISPLAY_WIDTH / 2, 0, "Team 310");
-    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.drawString(DISPLAY_WIDTH / 2, 0, "HAL 9310");
+    //display.setTextAlignment(TEXT_ALIGN_RIGHT);
     //display.drawString(DISPLAY_WIDTH, 0, String(voltage) + "V");
     display.setTextAlignment(TEXT_ALIGN_CENTER);
     int batLevel = round(3.0 * (voltage - minVoltage) / (maxVoltage - minVoltage));
     drawBattery(0, 1, batLevel);
 
     if (WiFi.softAPgetStationNum() > 0) {
-      display.drawXbm(95, 0, 9, 9, wifi3_bits);
       display.drawXbm(106, 0, 11, 9, wifi5_bits);
     }
 
-    if (lights)
+    if (lights == 0)
       display.drawXbm(119, 0, 9, 9, moon1_bits);
-    else
+    else if (lights == 1)
       display.drawXbm(119, 0, 9, 9, sun_bits);
+    else
+      display.drawXbm(119, 0, 9, 9, A_bits);
 
-    drawArrow(1 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT / 2 + 8, movement);
-
-    //display.drawXbm(0, 10, 64, 50, Auto);
+    if (movement < 4)
+      drawArrow(1 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT / 2 + 8, movement);
+    else
+      display.drawXbm(0, 13, Auto_width, Auto_height, Auto_bits);
 
     drawMeter(3 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT / 2 + 12, voltage / maxVoltage, 27);
     display.drawString(3 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT - 12, String(voltage * 3));
@@ -80,9 +84,10 @@ void loop() {
   }
 
   if (millis() > nextInterval) {
-    lights = !lights;
+    lights += 1;
+    lights %= 3;
     movement += 1;
-    movement %= 4;
+    movement %= 8;
     nextInterval = millis() + interval;
   }
 }
