52dd1e6926b567abf4735379f6ce4ab02f8ea4ca
Pieter Pas <pieter.pas@outlook.com>
2017-03-25 12:14:19 +0100

    Comments added to OLED sketches

--- Arduino/OLED_schermpje/OLED_test/OLED_test.ino
+++ Arduino/OLED_schermpje/OLED_test/OLED_test.ino
@@ -1,42 +1,36 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 
-#include "moon1.h"
+#include "moon1.h" // include some XBM images
 #include "sun.h"
 //#include "wifi3.h"
 #include "wifi5.h"
 #include "A.h"
 #include "Auto.h"
 
-#include "OLED.h"
+#include "OLED.h" // some custom display functions
 #include "NetworkInit.h"
 
-const float R1 = 71000;
+const float R1 = 71000; // values of the voltage divider to measure the battery voltage
 const float R2 = 9500;
 const float ResRatio = R2 / (R1 + R2);
 
-const float minVoltage = 2.4;
-const float maxVoltage = 3.27;
+const float minVoltage = 2.4; // minimum battery voltage
+const float maxVoltage = 3.27; // voltage of fully charged battery
 
 const char *ssid         = "Team 310";
 const char *password     = "pieter2016";
 
 void setup() {
   Serial.begin(115200);
-  delay(200);
-  Serial.println("Setup");
+  delay(20);
+  Serial.println("\n\r\nSetup");
   WiFi.mode(WIFI_AP);
-  WiFi.softAP (ssid, password);
+  WiFi.softAP (ssid, password); // Start a Wi-Fi access point
 
-  display.init();
-  display.flipScreenVertically();
-  display.setContrast(255);
+  startDisplay();
 
-  display.clear();
-  display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.setFont(ArialMT_Plain_10);
-
-  startOTA();
+  startOTA(); // start the Over The Air update services
 }
 
 unsigned long refresh = 50;
@@ -49,41 +43,44 @@ int lights = 0;
 int movement = 0;
 
 void loop() {
-  ArduinoOTA.handle();
+  ArduinoOTA.handle(); // check for over the air updates
+
   if (millis() > nextRefresh) {
-    display.clear();
-    float voltage = analogRead(A0) / ResRatio / 1024.0;
+    display.clear(); // clear the frame buffer
+
     display.drawString(DISPLAY_WIDTH / 2, 0, "HAL 9310");
+
+    float voltage = analogRead(A0) / ResRatio / 1024.0;
     //display.setTextAlignment(TEXT_ALIGN_RIGHT);
     //display.drawString(DISPLAY_WIDTH, 0, String(voltage) + "V");
-    display.setTextAlignment(TEXT_ALIGN_CENTER);
-    int batLevel = round(3.0 * (voltage - minVoltage) / (maxVoltage - minVoltage));
-    drawBattery(0, 1, batLevel);
+    //display.setTextAlignment(TEXT_ALIGN_CENTER);
+    int batLevel = round(3.0 * (voltage - minVoltage) / (maxVoltage - minVoltage)); // convert voltage to battery level
+    drawBattery(0, 1, batLevel); // show the battery level in the top bar
 
-    if (WiFi.softAPgetStationNum() > 0) {
-      display.drawXbm(106, 0, 11, 9, wifi5_bits);
+    if (WiFi.softAPgetStationNum() > 0) { // If there are stations connected to the access point
+      display.drawXbm(106, 0, 11, 9, wifi5_bits); // Show the Wi-Fi icon in the top bar
     }
 
-    if (lights == 0)
-      display.drawXbm(119, 0, 9, 9, moon1_bits);
-    else if (lights == 1)
-      display.drawXbm(119, 0, 9, 9, sun_bits);
-    else
-      display.drawXbm(119, 0, 9, 9, A_bits);
+    if (lights == 0) // if the lights are off
+      display.drawXbm(119, 0, 9, 9, moon1_bits); // show a moon symbol in the top bar
+    else if (lights == 1) // if the lights are on
+      display.drawXbm(119, 0, 9, 9, sun_bits); // show a sun symbol in the top bar
+    else // if the lights are in automatic mode
+      display.drawXbm(119, 0, 9, 9, A_bits); // show the letter A in the top bar
 
     if (movement < 4)
-      drawArrow(1 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT / 2 + 8, movement);
+      drawArrow(1 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT / 2 + 8, movement); // show an arrow indicating the direction of the movement
     else
-      display.drawXbm(0, 13, Auto_width, Auto_height, Auto_bits);
+      display.drawXbm(0, 13, Auto_width, Auto_height, Auto_bits); // Indicate that the wheelchair is navigating on autopilot
 
-    drawMeter(3 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT / 2 + 12, voltage / maxVoltage, 27);
-    display.drawString(3 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT - 12, String(voltage * 3));
+    drawMeter(3 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT / 2 + 12, voltage / maxVoltage, 27); // draw the speedometer (based on the voltage level)
+    display.drawString(3 * DISPLAY_WIDTH / 4, DISPLAY_HEIGHT - 12, String(voltage * 3)); // draw the number under the speed gauge
 
-    display.display();
+    display.display(); // send the frame buffer to the display
     nextRefresh = millis() + refresh;
   }
 
-  if (millis() > nextInterval) {
+  if (millis() > nextInterval) { // increment some of the variables to test the different states and symbols
     lights += 1;
     lights %= 3;
     movement += 1;
